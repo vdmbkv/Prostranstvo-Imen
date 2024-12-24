4ea87bd871d2f55b9868a821a68131d13d47115a
vdmbkv <lpnight2@gmail.com>
2024-12-24 12:13:06 +0300

    Add files via upload

---
+++ daw.py
@@ -0,0 +1,11 @@
+# Пространство имен
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+test_function()
+
+# inner_function() - конечно же выйдет ошибка, тк функция локальная
